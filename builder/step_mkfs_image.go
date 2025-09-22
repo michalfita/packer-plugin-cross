108409fef4fb78a7d564338123f296a31c69303c
Michał Fita <4925040+michalfita@users.noreply.github.com>
2025-09-22 14:53:16 +0100

    [Fixes #72] Add loopback partition presense check with single delay

--- builder/step_mkfs_image.go
+++ builder/step_mkfs_image.go
@@ -2,9 +2,12 @@ package builder
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	"github.com/hashicorp/packer-plugin-sdk/packer"
@@ -24,6 +27,15 @@ func (s *StepMkfsImage) Run(_ context.Context, state multistep.StateBag) multist
 	for i, partition := range config.ImageConfig.ImagePartitions {
 		cmd := fmt.Sprintf("mkfs.%s", partition.Filesystem)
 		args := append(partition.FilesystemMakeOptions, fmt.Sprintf("%sp%d", loopDevice, i+1))
+		
+		if _, err := os.Stat(args[1]); errors.Is(err, os.ErrNotExist) {
+			ui.Error(fmt.Sprintf("partition loopack `%s` doesn't exist immediately after mouting, delaying...", args[1])) // TODO: where's warning?
+  			time.Sleep(1 * time.Second)
+			if _, err := os.Stat(args[1]); errors.Is(err, os.ErrNotExist) {
+				ui.Error(fmt.Sprintf("partition loopack `%s` doesn't exist, aborting partition creation", args[1]))
+				return multistep.ActionHalt
+			}
+		}
 
 		ui.Message(fmt.Sprintf("creating partition #%d: mkfs.%s %s", i+1, cmd, strings.Join(args, " ")))
 		out, err := exec.Command(cmd, args...).CombinedOutput()
