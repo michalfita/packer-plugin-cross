87955e479d17f23e686a2f01070dfbb4f9d169ed
Michał Fita <4925040+michalfita@users.noreply.github.com>
2025-09-22 15:07:41 +0100

    Fix the index bug + add new demo board

--- builder/step_mkfs_image.go
+++ builder/step_mkfs_image.go
@@ -26,13 +26,14 @@ func (s *StepMkfsImage) Run(_ context.Context, state multistep.StateBag) multist
 
 	for i, partition := range config.ImageConfig.ImagePartitions {
 		cmd := fmt.Sprintf("mkfs.%s", partition.Filesystem)
-		args := append(partition.FilesystemMakeOptions, fmt.Sprintf("%sp%d", loopDevice, i+1))
-		
-		if _, err := os.Stat(args[1]); errors.Is(err, os.ErrNotExist) {
-			ui.Error(fmt.Sprintf("partition loopack `%s` doesn't exist immediately after mouting, delaying...", args[1])) // TODO: where's warning?
+		loopPartition := fmt.Sprintf("%sp%d", loopDevice, i+1)
+		args := append(partition.FilesystemMakeOptions, loopPartition)
+
+		if _, err := os.Stat(loopPartition); errors.Is(err, os.ErrNotExist) {
+			ui.Error(fmt.Sprintf("partition loopack `%s` doesn't exist immediately after mouting, delaying...", loopPartition)) // TODO: where's warning?
   			time.Sleep(1 * time.Second)
 			if _, err := os.Stat(args[1]); errors.Is(err, os.ErrNotExist) {
-				ui.Error(fmt.Sprintf("partition loopack `%s` doesn't exist, aborting partition creation", args[1]))
+				ui.Error(fmt.Sprintf("partition loopack `%s` doesn't exist, aborting partition creation", loopPartition))
 				return multistep.ActionHalt
 			}
 		}
